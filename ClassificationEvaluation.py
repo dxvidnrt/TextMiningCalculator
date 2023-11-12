bc971de10a05ca10dabc2c82391a8d7b2abfe329
dxvidnrt <david.niebert@googlemail.com>
2023-11-12 18:16:42 +0100

    Add one vs one Classification in ClassificationEvaluation.py

--- ClassificationEvaluation.py
+++ ClassificationEvaluation.py
@@ -1,4 +1,6 @@
+import math
 from typing import List
+
 
 def get_confusion_matrix(
         actual: List[int], predicted: List[int]
@@ -131,6 +133,60 @@ def false_negative_rate(actual: List[int], predicted: List[int]) -> float:
     """
     conf_matrix = get_confusion_matrix(actual, predicted)
     return conf_matrix[1][0] / (conf_matrix[1][0] + conf_matrix[1][1])
+
+
+def one_vs_rest_classification(instance_predictions: List[int]) -> str:
+    """Returns votes for each class given test instance using one-vs-rest voting scheme.
+
+    Args:
+        instance_predictions: List of predictions for given test instance.
+        Each element in the list corresponds to a prediction of a binary
+        classification. 1 is positive prediction for the classifier at that index and 0 is negative prediction.
+
+    Returns:
+        A list with the number of votes each class received."""
+    amount_classes = len(instance_predictions)
+    output = [0] * amount_classes
+
+    for i in range(amount_classes):
+        if instance_predictions[i]:
+            output[i] += 1
+        else:
+            output = [value + 1 if index != i else value for index, value in enumerate(output)]
+    print("One vs. Rest Classification: "+str(output))
+
+
+def one_vs_one_classification(instance_predictions: List[int]) -> str:
+    """Returns votes for each class given test instance using one-vs-rest voting scheme.
+
+        Args:
+            instance_predictions: List of predictions for given test instance.
+            Each element in the list corresponds to a prediction of a binary
+            classification. 1 is positive prediction for the classifier at that index and 0 is negative prediction.
+            Results are made for one pair (x_1, x_2), (x_1, x_3), ...
+
+
+        Returns:
+            A list with the number of votes each class received."""
+
+    # Function to iterate and group the values
+    n = len(instance_predictions)
+
+    amount_classes = 1/2*(math.sqrt(8*n+1)-1)+1
+    if not amount_classes.is_integer():
+        print("Error in list length")
+        return
+    amount_classes = int(amount_classes)
+    output = [0] * amount_classes
+    index = 0
+    for i in range(amount_classes):
+        for j in range(i+1, amount_classes):
+            if instance_predictions[index]:
+                output[i] += 1
+            else:
+                output[j] += 1
+            index += 1
+    print("One vs. One Classification: " + str(output))
 
 
 def classification_evaluation(actual: List[int], predicted: List[int]):
