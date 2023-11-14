ff85edef5857d78c3f2098b5c66054b9665b037c
dxvidnrt <david.niebert@googlemail.com>
2023-11-14 15:20:05 +0100

    Add CrossValidation for k-folds

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ClassificationEvaluation
+import CrossValidator
 import InvertedIndex
 import Retrieval
 import PageRanker
@@ -10,8 +11,8 @@ def main():
     classification_evaluation_actual = [0, 1, 1, 0, 0, 0, 1, 1, 0, 0]
     classification_evaluation_predicted = [1, 1, 1, 1, 0, 0, 1, 0, 0, 1]
     # endregion
-    ClassificationEvaluation.classification_evaluation(classification_evaluation_actual,
-                                                       classification_evaluation_predicted)
+    # ClassificationEvaluation.classification_evaluation(classification_evaluation_actual,
+    # classification_evaluation_predicted)
 
     # region Creating an inverted index
     inverted_index_doc1 = "The old man and his two sons went fishing."
@@ -20,21 +21,20 @@ def main():
     inverted_index_doc4 = "Introduction to how lake trout fishing works."
     stopwords = "an, and, are, for, how, in, is, not, or, the, these, this, to, with"
     # endregion
-    InvertedIndex.inverted_index_representation([inverted_index_doc1, inverted_index_doc2, inverted_index_doc3,
-                                                inverted_index_doc4], stopwords=stopwords, suffix_s_removal=True)
+    # InvertedIndex.inverted_index_representation([inverted_index_doc1, inverted_index_doc2, inverted_index_doc3,
+    # inverted_index_doc4], stopwords=stopwords, suffix_s_removal=True)
 
     # region Classification Evaluation
-    #endregion
-    ClassificationEvaluation.one_vs_rest_classification([1, 1, 1, 1])
-    ClassificationEvaluation.one_vs_one_classification([1, 1, 0, 1, 0, 1])
+    # endregion
+    # ClassificationEvaluation.one_vs_rest_classification([1, 1, 1, 1])
+    # ClassificationEvaluation.one_vs_one_classification([1, 1, 0, 1, 0, 1])
 
     # region Ranking Evaluation
     docs = [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0]
     # endregion
-    Retrieval.precision_at_k(docs, 6, 10)
-    Retrieval.recall_at_k(docs, 6, None)
-    Retrieval.average_precision(docs, 6)
-
+    # Retrieval.precision_at_k(docs, 6, 10)
+    # Retrieval.recall_at_k(docs, 6, None)
+    # Retrieval.average_precision(docs, 6)
 
     # region BM25 Retrieval
     doc = {
@@ -47,26 +47,32 @@ def main():
     }
     query = [("T2", 50), ("T2", 50), ("T5", 100)]
     # endregion
-    Retrieval.bm_25(doc, query, 1000, 50, 1.25, 0.8)
+    # Retrieval.bm_25(doc, query, 1000, 50, 1.25, 0.8)
 
     # region Bigrams
     term_1 = "A"
     term_2 = "B"
     doc = ["A", "B", "C", "A", "B"]
     # endregion
-    Retrieval.count_ordered_bigrams(term_1, term_2, doc)
-    Retrieval.count_unordered_bigrams(term_1, term_2, doc)
+    # Retrieval.count_ordered_bigrams(term_1, term_2, doc)
+    # Retrieval.count_unordered_bigrams(term_1, term_2, doc)
 
     # region Similarity
     term_vector_1 = [1, 1, 0, 2]
     term_vector_2 = [1, 1, 2, 0]
     # endregion
-    Similarity.jaccard(term_vector_1, term_vector_2)
+    # Similarity.jaccard(term_vector_1, term_vector_2)
 
     # region PageRanker
     graph_edges = [("A", "B"), ("B", "C"), ("C", "E"), ("E", "D"), ("D", "B"), ("E", "F")]
     # endregion
-    PageRanker.pagerank(graph_edges, q=0.4, iterations=3)
+    # PageRanker.pagerank(graph_edges, q=0.4, iterations=3)
+
+    # region CrossValidation k-fold
+    instances = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    k = 3
+    # endregion
+    # CrossValidator.create_folds(instances, k)
 
 
 if __name__ == "__main__":
